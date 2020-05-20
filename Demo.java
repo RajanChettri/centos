cf3190aa729efb87d3a54afe15ff007f32accd93
rajanchettri <rajan_chettri@live.com>
2020-05-20 13:24:32 +0530

    12

--- Demo.java
+++ Demo.java
@@ -1,10 +1,7 @@
 class Demo{
 
 	public static void main(String[] args){
-<<<<<<< HEAD
-	System.out.println("WebHooks Localhost! New 10");
-=======
-	System.out.println("WebHooks Localhost! New 12");
->>>>>>> 1af82e4bc9dade01cf9b05eaed2ee110188703cd
+
+	System.out.println("WebHooks Localhost! New ");
 }
 }
